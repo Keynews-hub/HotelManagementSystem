6e165a51bbd5d4166c4e7454755548c11ae5a773
yusril arzaq <yusrilarzaq@gmail.com>
2026-01-20 01:05:58 +0700

    add message info

--- HotelManagementSystem/FrmLogin.cs
+++ HotelManagementSystem/FrmLogin.cs
@@ -19,6 +19,8 @@ namespace HotelManagementSystem
             InitializeComponent();
         }
 
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\yusri\OneDrive\Dokumen\Hotel.mdf;Integrated Security=True;Connect Timeout=30";
+
         private void btn_Register_Click(object sender, EventArgs e)
         {
             FrmRegistrasi registrasi = new FrmRegistrasi();
@@ -28,7 +30,44 @@ namespace HotelManagementSystem
 
         private void btn_Login_Click(object sender, EventArgs e)
         {
+            string username = txt_Username.Text;
+            string password = txt_Passwoord.Text;
 
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                using (SqlConnection connect = new SqlConnection(connection))
+                {
+                    connect.Open();
+                    string selectData = "SELECT * FROM users WHERE (username = @usern AND password = @pass) AND status = 'Active'";
+
+                    using (SqlCommand comand = new SqlCommand(selectData, connect))
+                    {
+                        comand.Parameters.AddWithValue("@usern", username.Trim());
+                        comand.Parameters.AddWithValue("@pass", password.Trim());
+                        comand.Parameters.AddWithValue("@status", "Active");
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(comand);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+
+                        if (table.Rows.Count != 0)
+                        {
+                            MessageBox.Show("Login Successfuly", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Incorrect Username/Password", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
